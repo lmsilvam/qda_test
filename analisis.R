0a55b9a97a7acc8319c62c3796b58e97d55e2c40
Luis Silva <lmsilvam@zoho.com>
2023-08-03 12:07:31 -0500

    First commit

---
+++ analisis.R
@@ -0,0 +1,46 @@
+# Importar paquetes
+if (!require(tidytext)) {
+  install.packages(tidytext)
+  library(tidytext)
+}
+if (!require(tidyverse)) {
+  install.packages(tidyverse)
+  library(tidyverse)
+}
+if (!require(quanteda)) {
+  install.packages(quanteda)
+  library(quanteda)
+}
+
+
+# Leer archivos
+temp_df <- data.frame(matrix(nrow=1, ncol=2))
+colnames(temp_df) <- c("nombre", "contenido")
+archivos <- list.files(path='discursos', pattern="\\.txt$", full.names = T)
+cuantos <- length(archivos)
+for (i in 1:cuantos) {
+  contenido_arch <- read_file(archivos[i])
+  nombre_arch <- substr(archivos[i], 11, 15)
+  temp_df <- rbind(temp_df, c(nombre_arch, contenido_arch))
+}
+temp_df <- temp_df[-1, ]
+
+# Discursos de ambos, juntos
+petro <- paste(temp_df[temp_df$nombre=="petro",2], collapse=" ")
+duque <- paste(temp_df[temp_df$nombre=="duque",2], collapse=" ")
+
+# Stopwords en español
+custom_stop_words <- bind_rows(stop_words,
+                               data_frame(word = quanteda::data_char_stopwords$spanish,
+                                          lexicon = "custom"))
+
+# Convertir en tokens todo
+df_petro <- tibble(contenido=petro) %>% unnest_tokens(output=palabra, input=contenido)
+df_duque <- tibble(contenido=duque) %>% unnest_tokens(output=palabra, input=contenido)
+
+# Conteo de palabras
+df_petro %>%
+  count(palabra) %>%
+  ggplot(aes(x = palabra,
+             y = n)) +
+  geom_col()
