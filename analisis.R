f9dc9f3594e89b59e3cb8030a89135e50c69b6a9
Luis Silva <lmsilvam@zoho.com>
2023-08-03 12:56:46 -0500

    Wordclouds!

--- analisis.R
+++ analisis.R
@@ -7,11 +7,14 @@ if (!require(tidyverse)) {
   install.packages(tidyverse)
   library(tidyverse)
 }
-if (!require(quanteda)) {
-  install.packages(quanteda)
-  library(quanteda)
+if (!require(patchwork)) {
+  install.packages('patchwork')
+  library(patchwork)
 }
-
+if (!require(wordcloud)) {
+  install.packages('wordcloud')
+  library(wordcloud)
+}
 
 # Leer archivos
 temp_df <- data.frame(matrix(nrow=1, ncol=2))
@@ -30,7 +33,7 @@ petro <- paste(temp_df[temp_df$nombre=="petro",2], collapse=" ")
 duque <- paste(temp_df[temp_df$nombre=="duque",2], collapse=" ")
 
 # Stopwords en español
-custom_stop_words <- readLines("stopwords-es.txt", warn=F)
+custom_stop_words <- data.frame(palabra=readLines("stopwords-es.txt", warn=F), lexicon="SMART")
 
 # Convertir en tokens todo
 df_petro <- tibble(contenido=petro) %>% 
@@ -41,8 +44,32 @@ df_duque <- tibble(contenido=duque) %>%
   anti_join(custom_stop_words)
 
 # Conteo de palabras
-df_petro %>%
-  count(palabra) %>%
-  ggplot(aes(x = palabra,
+ppal_petro <- df_petro %>% count(palabra) %>% filter(n>10) %>% arrange(desc(n)) 
+ppal_duque <- df_duque %>% count(palabra) %>% filter(n>10) %>% arrange(desc(n)) 
+
+p1 <- ppal_petro %>%
+  head(20) %>% 
+  ggplot(aes(x = reorder(palabra, n),
              y = n)) +
-  geom_col()
+  geom_col() + coord_flip() + labs(title="Petro", x="Palabra", y="Conteo")
+
+p2 <- ppal_duque %>%
+  head(20) %>% 
+  ggplot(aes(x = reorder(palabra, n),
+             y = n)) +
+  geom_col() + coord_flip() + labs(title="Duque", x="Palabra", y="Conteo")
+
+p1+p2
+
+# Nube de palabras
+wordcloud(words= ppal_petro$palabra,
+          freq= ppal_petro$n,
+          random.order = F,
+          max.words=45)
+
+wordcloud(words= ppal_duque$palabra,
+                freq= ppal_duque$n,
+                random.order=F,
+                max.words=45)
+
+
